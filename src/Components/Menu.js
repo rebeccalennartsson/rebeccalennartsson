5ab4305d63fdb1bc104d897df440b3d4c96f40f9
Jeremy Danner <jeremydanner2@gmail.com>
2022-10-01 10:27:07 +0200

    updated menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 import './Menu.scss';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { PagesContext } from '../Server/PagesContext';
 
 /**
@@ -28,20 +28,42 @@ const MenuItems = (props) => {
     return (
         <div className="topnav">
             {links}
-            <a onClick={closeLinks}>X</a>
+            <a href="#close-menu" onClick={closeLinks}>X</a>
         </div>
     );
 };
 
 export const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isInView, setIsInView] = useState(false);
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setIsInView((window.innerHeight - 75) < position);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const style = {
+        'backgroundColor': '#dddddd',
+        'boxShadow': '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+        'transition': 'all 0.3s cubic-bezier(.25,.8,.25,1)',
+        'opacity': '0.9'
+    };
+
     return (
         <div>
             <MenuItems isOpen={isOpen} closeLinks={() => setIsOpen(false)} />
             {isOpen ? null : (
-                <a className="menu-toggle" onClick={() => setIsOpen(true)}>
-                    =
-                </a>
+                <div style={isInView ? style : {}} className="menu-toggle">
+                    <h1 style={{'color': isInView ? '#333333' : ''}}>Rebecca Lennartsson</h1>
+
+                    <a style={{'color': isInView ? '#dddddd' : '', 'backgroundColor': isInView ? '#333333' : ''}} href="#menu" onClick={() => setIsOpen(true)}>
+                        =
+                    </a>
+                </div>
             )}
         </div>
     );
